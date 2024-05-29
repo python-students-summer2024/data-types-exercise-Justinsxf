0408ad3c4b1dff3b255158d3947f49e4d88e8a2b
“Justinsxf”.  <XS2456@nyu.edu>
2024-05-28 22:40:12 -0400

    0731

--- solutions.py
+++ solutions.py
@@ -11,6 +11,10 @@ def calculate_profit():
     You can assume the user will enter only numeric characters, e.g. "3000", not "$3,000.00"
     The output should match the format of the following examples: "Profit: $690.00" for sales of $3,000, or "Profit: $2,300.00" for sales of $10,000, etc.
     """
+    total_sales = float(input("enter your annual profit?"))
+    profit=total_sales*0.23
+    print(f"Profit: ${profit:,.2f}")
+
 
 
 def calculate_quotient_and_remainder():
@@ -22,6 +26,15 @@ def calculate_quotient_and_remainder():
       Enter number #2: 2
       2 goes into 5 a total of 2 times with a remainder of 1
     """
+    num1 = int(input("enter number #1"))
+    num2 = int(input("enter number #2"))
+    quotient = num1//num2
+    remainder = num1 % num2
+
+    print(f"{num2} goes into {num1} a total of {quotient} times with a remainder of {remainder}")
+
+
+ 
 
 
 def calculate_miles_per_gallon():
@@ -34,13 +47,23 @@ def calculate_miles_per_gallon():
       Miles driven: 100
       Gas used (gallons): 25
       Miles per gallon: 2.2
+
     """
+    Miles_driven = int(input("enter your miles driven"))
+    Gas_used = int(input("enter your gas used"))
+
+    MPG = Miles_driven // Gas_used
+
+    print(f"Miles per gallon: {MPG:.1f}")
+
+
 
 
 def align_text():
     """
     Complete this function such that it asks the user to enter in 3 price values (as floating point numbers).
-    The print out the price values so that they are formatted to two decimal places. Also make sure that the price values are right aligned and line up at the decimal point.
+    The print out the price values so that they are formatted to two decimal places. Also make sure that the price 
+    values are right aligned and line up at the decimal point.
     Here's a sample running of the program:
 
       Enter price #1: 1.55
@@ -53,3 +76,12 @@ def align_text():
       Price #2: $   10.00
       Price #3: $ 9532.60
     """
+
+    num1 = float(input("Enter price #1"))
+    num2 = float(input("Enter price #2"))
+    num3 = float(input("ENter price #3"))
+
+    print("\nHere are your prices!\n")
+    print(f"Price #1: ${num1:10.2f}")
+    print(f"Price #2: ${num2:10.2f}")
+    print(f"Price #3: ${num3:10.2f}")
